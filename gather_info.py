e30493aa6b248511465c2cde7e7b97de6703efca
Alex Nemecek <an440818@ohio.edu>
2023-03-19 00:09:33 -0400

    cleanup deleted files

--- gather_info.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-import numpy as np
-import glob
-
-for infile in glob.glob("./CSVs/*.csv"):
-    if infile == "./CSVs/UDPLag_01-12.csv":
-        for chunk in pd.read_csv(infile, chunksize=100000): 
-            print(chunk[' Label'].value_counts())
